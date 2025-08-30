6c1b89448b09f67b26bdc860dab3852e87c6cde6
CandelaStaccia <candela.staccia@gmail.com>
2025-08-30 12:38:44 -0300

    listo con comentarios

--- src/filtros/validaciones.js
+++ src/filtros/validaciones.js
@@ -1,1 +1,28 @@
+const validaciones = [
+    {
+        nombre: "stock",
+        fn: (p, v) => p.stock > v
+    },
+    {
+        nombre: "precio",
+        fn: (p, v) => p.precio <= v
+    },
+    {
+        nombre: "nombre",
+        fn: (p, v) => p.nombre.length < v
+    },
+    {
+        nombre: "categoria",
+        fn: (p, arr) => arr.includes(p.categorias)
+    },
+    {
+        nombre: "precioRango",
+        fn: (p, v) => v.min <= p.precio && p.precio <= v.max
+    },
+    {
+        nombre: "stockRango",
+        fn: (p, v) => v.min <= p.stock && p.stock <= v.max
+    }
+]
 
+module.exports = validaciones
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+const express = require("express")
+const productos = require("../data/productos.json")
+const validaciones = require("./filtros/validaciones")
 
+const app = express()
+app.use(express.json())
+
+
+app.post("/productos/filtrar", (req, res) => {
+    const filtros = req.body
+    const cumpleFiltros = (producto, filtros) => {
+        return filtros.every(f => {
+            const validacion = validaciones.find(v => v.nombre === f.fn) //busco la validacion
+            return validacion ? validacion.fn(producto, f.values) : false //si existe, evalua
+        })
+    }
+    const result = productos.filter(p => cumpleFiltros(p, filtros))
+    //filtro los productos que cumplen con todos los filtros enviados en el body.
+    //toma un producto, busco la validacion del 1er filtro que mandaron, cumple, evaluo el siguiente filtro, cumple, agrega 
+    // ese producto al array de los que cumplen y pasa al siguiente producto para hacer lo mismo
+
+    res.status(201).json(result)
+})
+
+
+
+app.listen(3001, () => {
+    console.log("App iniciada en el puerto 3001")
+})
